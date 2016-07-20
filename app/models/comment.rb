38087222186004b733db1e80037cd07d7a9e6f45
Laura Rumage <laura.rumage@gmail.com>
2016-07-20 13:38:37 -0500

    Finish writing associations. Now testing...

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
+  belongs_to :author, class_name: :User
+  has_many :votes, as: :votable
+  belongs_to :commentable, polymorphic: true
 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :author, class_name: :User
   has_many :votes, as: :votable
-
+  has_many :comments, as: :commentable
+  has_many :answers
+  belongs_to :author, class_name: :User
 end
