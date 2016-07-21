d516124a2e0ee88eda66076037d54a8fbb3f21e8
Joe Duran & Laura Rumage <pair+duranje.laurarumage@devbootcamp.com>
2016-07-21 13:47:51 -0500

    work on ajax calls

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,15 @@ $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
-
+  $("#new-answer").on("submit", function(event){
+    event.preventDefault();
+    var url = this.attr("action");
+    var data = this.serialize();
+    var method = this.attr("method");
+    $.ajax({
+      url: url + "?" + data,
+      method: "post"
+    })
+  })
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
