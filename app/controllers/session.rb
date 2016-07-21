de35438743844a9e28917ca49c6cfbd0c5902a6f
Joe Duran & Laura Rumage <pair+duranje.laurarumage@devbootcamp.com>
2016-07-21 11:40:55 -0500

    implement static answer form and comment forms

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -12,7 +12,8 @@ post "/sessions" do
 
   if user = User.authenticate(email_or_username, plaintext_password)
     session[:user_id] = user.id
-    redirect "/"
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#{params[:returnURL]}"
+    redirect params[:returnURL] || "/"
   else
     @errors = ["Please check your log in credentials"]
     erb :"sessions/new"
