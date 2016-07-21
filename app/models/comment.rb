624d17613ff54af6f896d2d36af2bde887d8df4c
Joe Duran & Laura Rumage <pair+duranje.laurarumage@devbootcamp.com>
2016-07-20 19:02:40 -0500

    Add answers and questions logic for views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :author, class_name: :User
   has_many :votes, as: :voteable
   belongs_to :commentable, polymorphic: true
+
+  def minutes_ago
+    ((Time.now-self.created_at) / 60).floor
+  end
+
 end
